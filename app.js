abf3908a13c3e7d6193c572feb11defd563ec6db
Mohd Yousuf <mdyousuf43201@gmail.com>
2026-01-08 16:55:23 +0530

    drag functionality added

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-console.log("Hello, World!");
-console.log('welcome to kanban board');
+const coloumns = document.querySelectorAll(".task-col");
+// console.log(coloumns);
+
+const tasks = document.querySelectorAll(".task");
+// console.log(tasks);
+
+let dragElement = null;
+
+tasks.forEach((task) => {
+  task.addEventListener("drag", (e) => {
+    e.preventDefault();
+    dragElement = task;
+  });
+});
+
+coloumns.forEach((coloumn) => {
+  coloumn.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    coloumn.classList.add("hover");
+  });
+  coloumn.addEventListener("drag", (e) => e.preventDefault());
+  coloumn.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    coloumn.classList.remove("hover");
+  });
+  coloumn.addEventListener("drop", (e) => {
+    e.preventDefault();
+    console.log("dropped", dragElement, coloumn);
+    coloumn.appendChild(dragElement);
+    coloumn.classList.remove("hover");
+  });
+});
