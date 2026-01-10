c8f64ac728dd6314ace8d940e96260a5bc5fc8f3
Mohd Yousuf <mdyousuf43201@gmail.com>
2026-01-10 22:46:07 +0530

    update js logic

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const coloumns = document.querySelectorAll(".task-col");
 const todoColoumn = document.getElementById("todo");
+const addCount = document.getElementById("task-count");
 // console.log(coloumns);
 const sumbitBtn = document.getElementById("submit");
 const modal = document.querySelector(".modal");
@@ -10,64 +11,71 @@ const tasks = document.querySelectorAll(".task");
 
 let dragElement = null;
 
-tasks.forEach((task) => {
-  task.addEventListener("drag", (e) => {
-    e.preventDefault();
-    dragElement = task;
-  });
+document.addEventListener("dragstart", (e) => {
+  if (e.target.classList.contains("task")) dragElement = e.target;
+  e.target.classList.add("dragging");
 });
-
+document.addEventListener("dragend", (e) => {
+  if (e.target.classList.contains("task"))
+    e.target.classList.remove("dragging");
+});
 coloumns.forEach((coloumn) => {
   coloumn.addEventListener("dragover", (e) => {
     e.preventDefault();
     coloumn.classList.add("hover");
   });
-  coloumn.addEventListener("drag", (e) => e.preventDefault());
   coloumn.addEventListener("dragleave", (e) => {
-    e.preventDefault();
     coloumn.classList.remove("hover");
   });
   coloumn.addEventListener("drop", (e) => {
-    e.preventDefault();
-    console.log("dropped", dragElement, coloumn);
+    // console.log("dropped", dragElement, coloumn);
     coloumn.appendChild(dragElement);
     coloumn.classList.remove("hover");
+    updateTaskCount();
   });
 });
 
+function updateTaskCount() {
+  coloumns.forEach((col) => {
+    const taskLength = col.querySelectorAll(".task").length;
+    console.log(taskLength);
+    const count = col.querySelector(".task-count");
+    if (count) count.textContent = taskLength;
+    return;
+  });
+}
+
 addTaskBtn.addEventListener("click", () => {
-  modal.classList.toggle("active");
-})
+  modal.classList.add("active");
+});
+bgModal.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
 
 sumbitBtn.addEventListener("click", () => {
-  const title = document.getElementById("task-title").value;
-  const des = document.getElementById("title-des").value;
+  const title = document.getElementById("task-title").value.trim();
+  const des = document.getElementById("title-des").value.trim();
 
-  if (title && des) {
-    const taskDiv = document.createElement("div");
-    taskDiv.classList.add("task");
-    taskDiv.setAttribute("draggable", "true");
-    taskDiv.innerHTML = 
-    `<h3>${title}</h3>
+  if (!title || !des) return;
+
+  const taskDiv = document.createElement("div");
+  taskDiv.classList.add("task");
+  taskDiv.setAttribute("draggable", "true");
+  taskDiv.innerHTML = `<h3>${title}</h3>
     <p>${des}</p>
     <button class="delete-btn">Delete</button>`;
 
+  todoColoumn.appendChild(taskDiv);
 
-    taskDiv.addEventListener("drag", (e) => {
-      e.preventDefault();
-      dragElement = taskDiv;
-    })
-    todoColoumn.appendChild(taskDiv);   
-    
-    modal.style.display = "none";
-    document.getElementById("task-title").value = "";
-    document.getElementById("title-des").value = "";
-}else if(!title || !des){
-    
-  }
-})
-bgModal.addEventListener("click", () => {
   modal.classList.remove("active");
-})
-
-
+  document.getElementById("task-title").value = "";
+  document.getElementById("title-des").value = "";
+  updateTaskCount();
+});
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-btn")) {
+    const task = e.target.closest("task");
+    task.remove();
+  }
+  updateTaskCount();
+});
